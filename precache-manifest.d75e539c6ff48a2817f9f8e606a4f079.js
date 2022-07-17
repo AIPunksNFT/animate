1c0041c6904717557f211a875010c2031973f0da
AIPunksNFT <aipunksnft@gmail.com>
2022-07-17 19:27:44 +1000

    Updates

---
+++ precache-manifest.d75e539c6ff48a2817f9f8e606a4f079.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eebd98a5f77604fec5b5",
+    "url": "/animate/static/css/main.f4263c30.chunk.css"
+  },
+  {
+    "revision": "eebd98a5f77604fec5b5",
+    "url": "/animate/static/js/main.eebd98a5.chunk.js"
+  },
+  {
+    "revision": "f3ba07ae8adf9c0824d2",
+    "url": "/animate/static/js/1.f3ba07ae.chunk.js"
+  },
+  {
+    "revision": "e99623171173bbad6006",
+    "url": "/animate/static/js/runtime~main.e9962317.js"
+  },
+  {
+    "revision": "cf324f8b512738116200684223fee679",
+    "url": "/animate/index.html"
+  }
+];
